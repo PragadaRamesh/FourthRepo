6b972b12b963c5a605254c1ecce5ca642dd6fc90
root <root@ip-172-31-65-222.ec2.internal>
2025-07-01 18:27:47 +0000

    all commits

---
+++ app.java
@@ -0,0 +1,5 @@
+ GIT makes merging super easy! 
+CONFLICTS generally araise when two people two people have changed the same lines in a 
+f
+ ile (or) if one developer deleted a file while another developer is working on the same file!
+ In this situation git cannot determine what is correct!
